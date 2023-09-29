d73d36c0c651aa20d4ff79c294ed066f9d6b8310
Pornchai.Rungsirijaratthong <Pornchai.Rungsirijaratthong@wundermanthompson.com>
2023-09-29 11:59:24 +0700

    add debug

--- src/webhook-event/webhook-event.controller.ts
+++ src/webhook-event/webhook-event.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { WebhookEventService } from './webhook-event.service';
 import { CreateWebhookEventDto } from './dto/create-webhook-event.dto';
 
 @Controller('webhook-event')
 export class WebhookEventController {
-  constructor(private readonly webhookEventService: WebhookEventService) {}
+  constructor(private readonly webhookEventService: WebhookEventService) { }
+  private readonly logger = new Logger(WebhookEventController.name);
 
   @Post()
   create(@Body() createWebhookEventDto: CreateWebhookEventDto) {
+    this.logger.debug("createWebhookEventDto created", JSON.stringify(createWebhookEventDto))
     this.webhookEventService.create(createWebhookEventDto);
     return null; //return nothing as it is not defined in https://developers.line.biz/en/docs/messaging-api/receiving-messages/
   }
