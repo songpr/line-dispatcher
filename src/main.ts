20e1ced0ddd82e03b1329dcb8dcdeb7c11c0e31b
Pornchai.Rungsirijaratthong <Pornchai.Rungsirijaratthong@wundermanthompson.com>
2023-09-29 11:45:46 +0700

    add more test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,
+    { logger: ['error', 'warn', 'debug'] });
   await app.listen(3000);
 }
 bootstrap();
