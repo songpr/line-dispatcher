e19ed8e5076dae6afc6dd82b28059611ae07a48a
Pornchai.Rungsirijaratthong <Pornchai.Rungsirijaratthong@wundermanthompson.com>
2023-09-29 17:07:47 +0700

    run using fastify

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule,
+    new FastifyAdapter(),
     {
       logger: ['error', 'warn', 'log', 'debug'],
     });
