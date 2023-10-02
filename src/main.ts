ef8a27956ba1deceab01b1510ee09b1b91c96f17
Songpr <songpr@gmail.com>
2023-10-02 22:14:52 +0700

    support .env.dev

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { LogLevel, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
+import { log } from 'console';
 
 async function bootstrap() {
+  //default log level is error, warn, log
+  // TODO: check if log level is valid
+  const logLevel = [...(new Set(['error', 'warn', ...(process.env.LOG_LEVEL || 'log').split(',')]))] as LogLevel[];
+
   //see https://docs.nestjs.com/faq/raw-body
   const app = await NestFactory.create<NestFastifyApplication>(AppModule,
     new FastifyAdapter(),
     {
-      logger: ['error', 'warn', 'log', 'debug',],
+      logger: logLevel,
       rawBody: true,
     });
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   //open api
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Line Dispatcher')
