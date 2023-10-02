f8762f494dc092090b8be603dfccbf459a063bd3
Songpr <songpr@gmail.com>
2023-10-02 17:21:27 +0700

    support x-line-signature guard

--- src/auth/line-signature/line-signature.guard.ts
+++ src/auth/line-signature/line-signature.guard.ts
@@ -1,11 +1,18 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class LineSignatureGuard implements CanActivate {
+  private readonly logger = new Logger(LineSignatureGuard.name);
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const request = context.switchToHttp().getRequest();
+    const xLineSignature = request.headers['x-line-signature'];
+    if (xLineSignature !== null && xLineSignature !== undefined) {
+      this.logger.debug("webhook-event:x-line-signature", xLineSignature)
+      return true;
+    }
+    return false;
   }
 }
--- src/webhook-event/webhook-event.controller.ts
+++ src/webhook-event/webhook-event.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body, Logger } from '@nestjs/common';
+import { Controller, UseGuards, Post, Body, Logger } from '@nestjs/common';
 import { WebhookEventService } from './webhook-event.service';
 import { CreateWebhookEventDto } from './dto/create-webhook-event.dto';
+import { LineSignatureGuard } from 'src/auth/line-signature/line-signature.guard';
 
 @Controller('webhook-event')
+@UseGuards(LineSignatureGuard)
 export class WebhookEventController {
   constructor(private readonly webhookEventService: WebhookEventService) { }
   private readonly logger = new Logger(WebhookEventController.name);
@@ -11,6 +13,6 @@ export class WebhookEventController {
   create(@Body() createWebhookEventDto: CreateWebhookEventDto) {
     this.logger.debug("createWebhookEventDto created", JSON.stringify(createWebhookEventDto))
     this.webhookEventService.create(createWebhookEventDto);
-    return null; //return nothing as it is not defined in https://developers.line.biz/en/docs/messaging-api/receiving-messages/
+    return ''; //return nothing as it is not defined in https://developers.line.biz/en/docs/messaging-api/receiving-messages/
   }
 }
