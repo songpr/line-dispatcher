7988f7a90b8e0eb4de3ab6121e825133d03b39c0
Songpr <songpr@gmail.com>
2023-10-02 16:43:01 +0700

    gen line-signature guard

---
+++ src/auth/line-signature/line-signature.guard.spec.ts
@@ -0,0 +1,7 @@
+import { LineSignatureGuard } from './line-signature.guard';
+
+describe('LineSignatureGuard', () => {
+  it('should be defined', () => {
+    expect(new LineSignatureGuard()).toBeDefined();
+  });
+});
